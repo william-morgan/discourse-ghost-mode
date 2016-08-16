a78fd8bcc099bf71348678af37806ad00872d926
William Morgan <william.morgan@uwalumni.com>
2016-08-15 21:43:58 -0500

    asset fix

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,6 @@
 # author: william morgan
 
 after_initialize do
-  register_asset "../assets/javascripts/discourse/templates/components/composer.hbs"
-  register_asset "../assets/javascripts/discourse/templates/components/composer-ghost-handle.hbs"
+  register_asset "/assets/javascripts/discourse/templates/components/composer.hbs"
+  register_asset "/assets/javascripts/discourse/templates/components/composer-ghost-handle.hbs"
 end
